79da18471cd488255f56f7f9f4f160cfabe87f92
jiyaagrawalgkt-sketch <jiyaagrawalgkt@gmail.com>
2026-03-17 13:20:46 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,17 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ******************** Operation **************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2+2)
+console.log(2+2)
